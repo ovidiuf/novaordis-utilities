e693a0738884c1b3dffc0426308728d5bb32b559
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-06-12 22:01:33 -0400

    added OSAddressImpl

---
+++ src/main/java/io/novaordis/utilities/address/OSAddressImpl.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.utilities.address;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 6/12/17
+ */
+public class OSAddressImpl extends AddressImpl implements OSAddress {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    /**
+     * @param address the string representation of the address in the format:
+     *                protocol://[username[:password]]@hostname[:port]
+     * @throws AddressException
+     */
+    public OSAddressImpl(String address) throws AddressException {
+
+        super(address);
+    }
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
