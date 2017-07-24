a4f125d958dca5e9647fb5e488dab38a35c7afca
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-07-24 14:23:08 -0700

    help utilities (1)

---
+++ src/main/java/io/novaordis/utilities/help/InLineHelp.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.utilities.help;
+
+/**
+ * Static utilities for in-line help.
+ *
+ * See https://kb.novaordis.com/index.php/Project_In-Line_Help
+ *
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 7/24/17
+ */
+public class InLineHelp {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
