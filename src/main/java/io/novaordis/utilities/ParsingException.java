3d9c8c7b70686f808da18cf34233eeb658d6b0dc
Ovidiu Feodorov <ovidiu@novaordis.com>
2017-09-09 22:56:30 -0700

    moved ParsingExceptions from Events API here

---
+++ src/main/java/io/novaordis/utilities/ParsingException.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.utilities;
+
+/**
+ * A parsing exception. Provides line number and position in line information related to where the parsing error
+ * occurred.
+ *
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 2/14/17
+ */
+public class ParsingException extends LineNumberException {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    public ParsingException(String message) {
+
+        super(message);
+    }
+
+    public ParsingException(String message, Throwable cause) {
+
+        super(message, cause);
+    }
+
+    public ParsingException(Long lineNumber, String message) {
+
+        super(lineNumber, null, message, null);
+    }
+
+    public ParsingException(Long lineNumber, Throwable cause) {
+
+        super(lineNumber, null, null, cause);
+    }
+
+    public ParsingException(Long lineNumber, String message, Throwable cause) {
+
+        super(lineNumber, null, message, cause);
+    }
+
+    public ParsingException(Long lineNumber, Integer positionInLine, String message) {
+
+        super(lineNumber, positionInLine, message, null);
+    }
+
+    public ParsingException(Long lineNumber, Integer positionInLine, String message, Throwable cause) {
+
+        super(lineNumber, positionInLine, message, cause);
+    }
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
---
+++ src/test/java/io/novaordis/utilities/ParsingExceptionTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2017 Nova Ordis LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.novaordis.utilities;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * @author Ovidiu Feodorov <ovidiu@novaordis.com>
+ * @since 2/14/17
+ */
+public class ParsingExceptionTest {
+
+    // Constants -------------------------------------------------------------------------------------------------------
+
+    // Static ----------------------------------------------------------------------------------------------------------
+
+    // Attributes ------------------------------------------------------------------------------------------------------
+
+    // Constructors ----------------------------------------------------------------------------------------------------
+
+    // Public ----------------------------------------------------------------------------------------------------------
+
+    // Tests -----------------------------------------------------------------------------------------------------------
+
+    @Test
+    public void constructor() throws Exception {
+
+        RuntimeException cause = new RuntimeException();
+        ParsingException e = new ParsingException(10L, 11, "test", cause);
+        assertEquals("test", e.getMessage());
+        assertEquals(cause, e.getCause());
+        assertEquals(10L, e.getLineNumber().longValue());
+        assertEquals(11, e.getPositionInLine().intValue());
+    }
+
+    // Package protected -----------------------------------------------------------------------------------------------
+
+    // Protected -------------------------------------------------------------------------------------------------------
+
+    // Private ---------------------------------------------------------------------------------------------------------
+
+    // Inner classes ---------------------------------------------------------------------------------------------------
+
+}
